e4604bdcecd9c7fbba477a116f98171176b8ffa4
Aman Kumar <amandsvv4006@gmail.com>
2025-09-16 23:10:30 +0530

    problem No. 746 MInimum cost for climbing stairs (leetcode peoclem)

---
+++ min_cost.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int minCost(vector<int> & A){
+    int n = A.size();
+    vector<int> dp(n);
+
+    dp[0] = A[0];
+    dp[1] = A[1];
+
+    for(int i = 2; i < n; i++){
+        dp[i] = A[i] + min(dp[i-1],dp[i-2]);
+    }
+
+    return min(dp[n-1], dp[n-2]);
+}
+
+int main(){
+    vector<int> costs = {1,0,2,1};
+
+    cout<<minCost(costs);
+    return 0;
+
+}
