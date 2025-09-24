fa6231315b32a1151e380572c7771e1bd00d9fd7
Aman Kumar <amandsvv4006@gmail.com>
2025-09-24 21:00:26 +0530

    Leetcode Problem 1910 Remove all occurences of a sub string in a string

---
+++ Remove_all_occurence.cpp
@@ -0,0 +1,33 @@
+// 1910. Remove All Occurrences of a Substring
+
+// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+
+// A substring is a contiguous sequence of characters in a string.
+#include <iostream>
+
+using namespace std;
+
+string removeOccurrences(string s, string part)
+{
+    while (true)
+    {
+        int strt = s.find(part);
+        if (strt > s.size())
+            break;
+        int end = part.size();
+        s.erase(strt, end);
+    }
+    return s;
+}
+int main()
+{
+    string s = "baacabcbhuiabcgtyabc";
+    string part = "abc";
+
+    s= removeOccurrences(s,part);
+    cout<<"String : "<<s<<endl;
+    return 0;
+}
