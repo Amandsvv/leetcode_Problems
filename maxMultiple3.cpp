a4ab18330f4a183bb84feaf00131b07d25e36648
Aman Kumar <amandsvv4006@gmail.com>
2025-09-29 21:48:13 +0530

    problem 628 Maximum Product of three number in nums Leetcode Problem

--- maxMultiple3.cpp
+++ maxMultiple3.cpp
@@ -1,4 +1,19 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include<algorithm>
 
 using namespace std;
+
+int maximumProduct(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    int n = nums.size() - 1;
+    int option1 = nums[n] * nums[n - 1] * nums[n - 2];
+    int option2 = nums[0] * nums[1] * nums[n];
+    return max(option1, option2);
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    cout<<maximumProduct(nums)<<endl;
+}
