971b665ea17eddc86fd8af1aa74cfd32f78acfc1
Aman Kumar <amandsvv4006@gmail.com>
2025-10-05 22:46:49 +0530

    Leetcode Problem 509 Fibonacci Number

---
+++ fibanoci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int fib(int n){
+    if(n == 0 || n == 1){
+        return n;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    cout<< "fibonacci till 5th term : "<<fib(5);
+    return 0; 
+}
