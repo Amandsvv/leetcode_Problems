7fa217f5a9ee68b7a1e50761b98ecd1738466b3f
Aman Kumar <amandsvv4006@gmail.com>
2025-10-11 21:59:40 +0530

    Problem 423 Intersection of array II

---
+++ intersection_arrayII.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2){
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    vector<int> ans;
+    int idx1 = 0, idx2 = 0;
+
+    while(idx1 < nums1.size() && idx2 < nums2.size()){
+        if(nums1[idx1] == nums2[idx2]){
+            ans.push_back(nums1[idx1]);
+            idx1++; idx2++;
+        }else if(nums1[idx1] < nums2[idx2]){
+            idx1++;
+        }else{
+            idx2++;
+        }
+    }
+
+    return ans;
+}
+int main(){ 
+    
+}
