4570593f95360dc9ca81fd3cb2ec6ce1528a8c6f
Aman Kumar <amandsvv4006@gmail.com>
2025-10-30 23:50:41 +0530

    Problem. 290 Word Pattern

---
+++ word_Pattern.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+
+        while (ss >> word)
+            words.push_back(word);
+
+        if (words.size() != pattern.size())
+            return false;
+
+        unordered_map<char, string> p2w;
+        unordered_map<string, char> w2p;
+
+        for (int i = 0; i < pattern.size(); ++i) {
+            char p = pattern[i];
+            string w = words[i];
+
+            if (p2w.count(p) && p2w[p] != w) return false;
+            if (w2p.count(w) && w2p[w] != p) return false;
+
+            p2w[p] = w;
+            w2p[w] = p;
+        }
+
+        return true;
+    }
+};
+
+int main (){
+
+}
