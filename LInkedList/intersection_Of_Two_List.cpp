ea7be5f1ecbb151430c8e145574e7a1863bb1357
Aman Kumar <amandsvv4006@gmail.com>
2025-11-28 23:33:31 +0530

    160. Intersection of Two Linked Lists

---
+++ LInkedList/intersection_Of_Two_List.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+
+        if (!headA || !headB)
+            return nullptr;
+
+        ListNode *a = headA;
+        ListNode *b = headB;
+
+        while (a != b)
+        {
+            a = (a == nullptr) ? headB : a->next;
+            b = (b == nullptr) ? headA : b->next;
+        }
+
+        return a; // can be nullptr OR the intersection node
+    }
+};
+int main()
+{
+}
