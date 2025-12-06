fa484a6733c14b6d0d5301f28612962d0030c081
Aman Kumar <amandsvv4006@gmail.com>
2025-12-06 20:24:56 +0530

    2185. Counting Words With a Given Prefix

---
+++ String/counting_prefix_of_words.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n = pref.size(), count = 0;
+
+        for(string word: words){
+            if(word.size() >= n){
+                if(pref == word.substr(0,n)) count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main (){
+    Solution obj;
+    vector<string> words = {"pay","attention","practice","attend"}; 
+    cout<<obj.prefixCount(words)<<endl;
+}
