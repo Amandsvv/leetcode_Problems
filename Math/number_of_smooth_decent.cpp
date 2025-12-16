33f89cc39af10e799779a80d275f3f0ac474f5c3
Aman Kumar <amandsvv4006@gmail.com>
2025-12-16 22:17:56 +0530

    2110. Number of Smooth Descent Periods of a Stock (mine sollution)

--- Math/number_of_smooth_decent.cpp
+++ Math/number_of_smooth_decent.cpp
@@ -19,7 +19,25 @@ public:
         return count;
     }
 };
-
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long i = 0, count = 0, n = prices.size();
+        long long st = 0, end = 1;
+        while(i < n-1){
+            while(i < n-1 && prices[i] - prices[i+1] == 1){
+                i++;
+                end++;
+            }
+            long long total = end - st;
+            count+= ((total*(total+1))/2);
+            i++;
+            st = end;
+            end++;
+        }
+        return i == n?count:count+1;
+    }
+};
 int main (){
     vector<int> prices = {3,2,1,4};
     Solution obj;
