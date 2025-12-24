1a6812ee4e2d294d9ad0ed7ca3642360801af526
Aman Kumar <amandsvv4006@gmail.com>
2025-12-24 22:07:05 +0530

    Problem. 438. Find All Anagrams in a String

---
+++ HashMAP/find_All_Anagram.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans = {};
+        int n = s.size();
+
+        unordered_map<char,int> freq;
+        for(char ch : p){
+            freq[ch]++;
+        }
+
+        unordered_map<char,int> seq;
+        int i = 0, j = 0;
+        for(;j < p.size(); j++){
+            seq[s[j]]++;
+        }
+        if(seq == freq) ans.push_back(0);
+        while(j < n){
+            seq[s[j]]++;
+            seq[s[i]]--;
+
+            if(seq[s[i]] == 0){
+                seq.erase(s[i]);
+            }
+            i++; j++;
+            if(seq == freq) ans.push_back(i);
+        }
+        return ans;
+    }
+};
+int main (){
+
+}
