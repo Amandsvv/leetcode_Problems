eb98855038f57c89b0fe60f1c2a4abbd537375b1
Aman Kumar <amandsvv4006@gmail.com>
2025-12-28 22:07:09 +0530

    Problem. 2485. Find the Pivot Integer

---
+++ Math/pivot_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int rightSum = n*(n+1)/2;
+        int leftSum = 0;
+        for(int i = 1; i <= n ; i++){
+            leftSum+=i;
+            if(leftSum == rightSum) return i;
+            rightSum-=i;
+        }
+        return -1;
+    }
+};
+int main (){
+
+}
