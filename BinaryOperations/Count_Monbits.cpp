975e4b2b704d0a0eb9ab10b507529b1b2c99affd
Aman Kumar <amandsvv4006@gmail.com>
2026-02-01 12:26:11 +0530

    Problem. 3827. Count Monobit Integers

---
+++ BinaryOperations/Count_Monbits.cpp
@@ -0,0 +1,25 @@
+// 3827. Count Monobit Integers
+// Solved
+// Easy
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer n.
+
+// An integer is called Monobit if all bits in its binary representation are the same.
+
+// Return the count of Monobit integers in the range [0, n] (inclusive).
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int countMonobit(int n) {
+       return static_cast<int>(log2(n+1)) + 1;
+    }
+};
+
+int main (){
+
+}
