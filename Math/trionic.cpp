31b2537b7d4b6760a8989d82d98d0f0fcd8ec808
Aman Kumar <amandsvv4006@gmail.com>
2026-02-03 23:38:32 +0530

    Problem. 3637. Trionic Array I

---
+++ Math/trionic.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int i = 1, n = nums.size();
+
+        if(n < 4) return false;
+
+        while(i < n && nums[i] > nums[i-1]) i++;
+        if(i == 1 || i == n) return false;
+
+        while(i < n && nums[i] < nums[i-1]) i++;
+        if(i == n) return false;
+
+        while(i < n && nums[i] > nums[i-1]) i++;
+
+        return i == n;
+    }
+};
+
+int main (){
+
+}
