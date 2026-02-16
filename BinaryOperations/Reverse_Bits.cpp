c7a4b0eeea53faf6852850b3b363238fb3ab3998
Aman Kumar <amandsvv4006@gmail.com>
2026-02-16 23:25:57 +0530

    Problem. 190. Reverse Bits

---
+++ BinaryOperations/Reverse_Bits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            res = (res << 1) | ((n >> i) & 1);
+        }
+        return res;
+    }
+};
+int main (){
+
+}
