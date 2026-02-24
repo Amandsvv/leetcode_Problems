579b5a62fa701a7fa8e988313ec2ba930502407d
Aman Kumar <amandsvv4006@gmail.com>
2026-02-24 12:39:31 +0530

    problem. 1022. Sum of Root To Leaf Binary Numbers

---
+++ Tree/Sum_Of_Leaf_Binary.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sum = 0;
+
+    void dfs(TreeNode* root, int curr){
+        if(!root) return;
+
+        // build binary number
+        curr = curr * 2 + root->val;
+
+        // leaf node
+        if(!root->left && !root->right){
+            sum += curr;
+            return;
+        }
+
+        dfs(root->left, curr);
+        dfs(root->right, curr);
+    }
+
+    int sumRootToLeaf(TreeNode* root) {
+        dfs(root, 0);
+        return sum;
+    }
+};
+int main (){
+
+}
