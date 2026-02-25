8f8edf19579ec4462aa64d188c8cb684856c9628
Aman Kumar <amandsvv4006@gmail.com>
2026-02-25 21:49:25 +0530

    Problem. 1356. Sort Integers by The Number of 1 Bits

---
+++ BinaryOperations/Sort_Arr_1Bit.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b){
+            int ca = __builtin_popcount(a);
+            int cb = __builtin_popcount(b);
+
+            if(ca == cb) return a < b;
+            return ca < cb;
+        });
+        return arr;
+    }
+};
+int main (){
+
+}
