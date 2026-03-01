c2b292e2c444768c112c334aa02d62e085c40a49
Aman Kumar <amandsvv4006@gmail.com>
2026-03-01 23:40:16 +0530

    Problem. 3857. Minimum Cost to Split into Ones

---
+++ Math/Minimum_Cost_To_Split_Ones.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int minCost(int n) {
+        int ans = (n * (n-1)/2);
+        return ans;
+    }
+};
+int main (){
+
+}
