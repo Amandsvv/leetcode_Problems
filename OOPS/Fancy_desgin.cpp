cd8314db1fef0a59d5da5388e27a831c78c0b3e9
Aman Kumar <amandsvv4006@gmail.com>
2026-03-15 23:10:24 +0530

    Problem. 1622. Fancy Sequence

---
+++ OOPS/Fancy_desgin.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Fancy {
+private:
+    const int mod = 1e9+7; 
+    vector<long long> nums = {};
+    long long a,b;
+
+    long long modPow(long long x, long long y, long long mod){
+        long long res = 1;
+        x = x%mod;
+        while(y > 0){
+            if(y %2 == 1){
+                res = (res * x) % mod;
+            }
+            y = y/2;
+            x=(x*x) % mod;
+        }
+        return res;
+    }
+public:
+    Fancy() : a(1), b(0) {
+        
+    }
+    
+    void append(int val) {
+        long long x = (val - b + mod) % mod;
+        this->nums.push_back((x * modPow(a,mod -2, mod)) % mod);
+    }
+    
+    void addAll(int inc) {
+        b = (b + inc) % mod;
+    }
+    
+    void multAll(int m) {
+        a = (a*m) % mod;
+        b = (b*m) % mod;
+    }
+    
+    int getIndex(int idx) {
+        if(idx >= nums.size() || idx < 0) return -1;
+        return (a * nums[idx] + b) % mod;
+    }
+};
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy* obj = new Fancy();
+ * obj->append(val);
+ * obj->addAll(inc);
+ * obj->multAll(m);
+ * int param_4 = obj->getIndex(idx);
+ */
+int main (){
+
+}
