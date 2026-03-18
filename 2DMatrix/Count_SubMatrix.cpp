0f258a5f924668f5a498e6895c8bf1a2fb44eb7e
Aman Kumar <amandsvv4006@gmail.com>
2026-03-19 01:11:11 +0530

    Problem. 3070. Count Submatrices with Top-Left Element and Sum Less Than k

---
+++ 2DMatrix/Count_SubMatrix.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int n = grid[0].size();
+        int m = grid.size();
+        for(int i = 0 ; i < m; i++){
+            for(int j = 1; j < n; j++){
+                grid[i][j] = grid[i][j-1] + grid[i][j];
+            }
+        }
+        for(int i = 1 ; i < m; i++){
+            for(int j = 0; j < n; j++){
+                grid[i][j] = grid[i-1][j] + grid[i][j];
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            int j = n-1;
+            while(j >= 0 && grid[i][j] > k) j--;
+            ans += j+1;
+        }
+        return ans;
+    }
+};
+int main ()
+{}
