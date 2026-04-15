0056dcb54bf9a13e5c3075737f080718a3d22353
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-04-15 15:43:31 +0530

    Time: 0 ms (100%), Space: 18.2 MB (68.33%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-        int a = 0, b = 0;
+        int n = words.size();
+        for(int i = 0; i <= n >> 1; i++){
+            if(words[((startIndex + i)%n)] == target || words[((startIndex - i  + n)%n)] == target) return i;
+        }
+        return -1;
+        // int a = 0, b = 0;
 
-        int i = startIndex, n = words.size();
-        do {
-            if (words[i] == target)
-                break;
-            a++;
-            i = (i + 1) % n;
-        } while (i != startIndex);
+        // int i = startIndex, n = words.size();
+        // do {
+        //     if (words[i] == target)
+        //         break;
+        //     a++;
+        //     i = (i + 1) % n;
+        // } while (i != startIndex);
 
-        int j  = startIndex;
-        do {
-            if (words[j] == target)
-                break;
-            b++;
-            j = (j - 1 + n) % n;
-        } while (j != startIndex);
+        // int j  = startIndex;
+        // do {
+        //     if (words[j] == target)
+        //         break;
+        //     b++;
+        //     j = (j - 1 + n) % n;
+        // } while (j != startIndex);
 
-        if(words[i] != target && words[j] != target) return -1;
-        return min(a,b);
+        // if(words[i] != target && words[j] != target) return -1;
+        // return min(a,b);
     }
 };
