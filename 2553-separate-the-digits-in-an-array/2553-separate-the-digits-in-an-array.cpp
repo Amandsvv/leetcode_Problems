a964d2d686694497909284950dfd20f77e3e52c0
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-05-11 21:43:06 +0530

    Time: 1 ms (64.74%), Space: 15.2 MB (77.65%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ans;
         for(auto & el : nums){
-            vector<int> temp;
-            while(el){
-                temp.push_back(el%10);
-                el/=10;
+            for(auto & c : to_string(el)){
+                ans.push_back(c-'0');
             }
-            reverse(temp.begin(), temp.end());
-            ans.insert(ans.end(), temp.begin(), temp.end());
         }
         return ans;
     }
