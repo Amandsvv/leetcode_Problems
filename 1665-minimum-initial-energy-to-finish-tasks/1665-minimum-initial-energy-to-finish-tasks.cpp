e98b0dad81549796752b87cd9bdcb3a3c0560b8a
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-05-12 16:18:27 +0530

    Time: 91 ms (23.3%), Space: 110.6 MB (18.93%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    bool helper(int mid, vector<vector<int>> tasks) {
-        int currEnergy = mid;
-        for (auto& vec : tasks) {
-            if (currEnergy < vec[1])
-                return false;
-            currEnergy -= vec[0];
-        }
-        return true;
-    }
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b) {
             return (a[1] - a[0]) > (b[1] - b[0]);
         });
 
-        int ans;
-        int st = 0;
-        int end = 0;
+        int ans = 0;;
+        int energy = 0;
         for (vector<int> vec : tasks) {
-            st += vec[0];
-            end += vec[1];
-        }
+            int actual = vec[0];
+            int minimum = vec[1];
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (helper(mid, tasks)) {
-                end = mid - 1;
-                ans = mid;
-            } else
-                st = mid + 1;
+            if(energy < minimum){
+                ans += (minimum - energy);
+                energy = minimum;
+            }
+            energy -= actual;
         }
 
         return ans;
