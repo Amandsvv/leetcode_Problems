07abfbb49fb0be62336e1a5b7b0de955fd26bc51
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-05-26 16:17:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (27.15%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int> lower(26,0);
-        vector<int> upper(26,0);
-        for(char ch : word){
-            if(ch >= 'a' && ch <= 'z'){
-                lower[ch - 'a']++;
-            }else{
-                upper[ch-'A']++;
-            }
+        unordered_set<char> seen;
+        for(char & ch : word){
+            seen.insert(ch);
         }
         int ans = 0;
-        for(int i = 0; i < 26; i++){
-            if(lower[i] == 0 || upper[i] == 0) continue;
-            ans++;
-        }
-        return ans;
+        for(char ch : seen){
+            if(ch >= 'a' && ch <= 'z' && seen.count(ch - 32)){
+                ans ++;
+            }
+       }
+       return ans;
     }
 };
