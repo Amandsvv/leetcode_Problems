6d5ccaa009af6479faeba2178c83d86d0183e7ba
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-06-06 12:49:19 +0530

    Time: 0 ms (100%), Space: 15.1 MB (63.06%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        if(1 == n) return {0};
+
+        int totalSum = 0;
+        for(int el : nums) totalSum += el;
+
+        vector<int> ans;
+        int leftSum = 0;
+        for(int el : nums){
+            ans.push_back(abs(leftSum - (totalSum -= el)));
+            leftSum+=el;
+        }
+        return ans;
+    }
+};
