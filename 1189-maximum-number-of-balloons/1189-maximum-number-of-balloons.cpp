17346aab4e48dc60118aff20a63beadfe56824bc
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-06-22 12:19:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (84.03%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -13,19 +13,27 @@ public:
 
         // return min(e,(min(min(a,b), min(c,d))));
 
-        int freq[5] = {0};
-        for(char ch: text){
-            if(ch == 'b') freq[0]++;
-            if(ch == 'a') freq[1]++;
-            if(ch == 'l') freq[2]++;
-            if(ch == 'o') freq[3]++;
-            if(ch == 'n') freq[4]++;
-        }
-        int ans = INT_MAX;
-        freq[2]/=2; freq[3]/=2;
-        for(int el : freq){
-            if(el < ans) ans = el;
-        }
-        return ans;
+        // int freq[5] = {0};
+        // for(char ch: text){
+        //     if(ch == 'b') freq[0]++;
+        //     if(ch == 'a') freq[1]++;
+        //     if(ch == 'l') freq[2]++;
+        //     if(ch == 'o') freq[3]++;
+        //     if(ch == 'n') freq[4]++;
+        // }
+        // int ans = INT_MAX;
+        // freq[2]/=2; freq[3]/=2;
+        // for(int el : freq){
+        //     if(el < ans) ans = el;
+        // }
+        // return ans;
+
+        int freq[26] = {};
+
+        for (char ch : text)
+            freq[ch - 'a']++;
+
+        return min({freq['b' - 'a'], freq['a' - 'a'], freq['l' - 'a'] / 2,
+                    freq['o' - 'a'] / 2, freq['n' - 'a']});
     }
 };
