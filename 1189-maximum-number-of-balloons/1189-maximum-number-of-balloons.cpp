58c61a96718f89bfb3515752a808e1bbe8994781
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-06-22 12:15:04 +0530

    Time: 0 ms (100%), Space: 8.8 MB (84.03%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char,int> freq;
-        for(char ch : text){
-            freq[ch]++;
-        }
-        int a = freq['b'];
-        int b = freq['a'];
-        int c = freq['l']/2;
-        int d = freq['o']/2;
-        int e = freq['n'];
+        // unordered_map<char,int> freq;
+        // for(char ch : text){
+        //     freq[ch]++;
+        // }
+        // int a = freq['b'];
+        // int b = freq['a'];
+        // int c = freq['l']/2;
+        // int d = freq['o']/2;
+        // int e = freq['n'];
 
-        return min(e,(min(min(a,b), min(c,d))));
+        // return min(e,(min(min(a,b), min(c,d))));
+
+        int freq[5] = {0};
+        for(char ch: text){
+            if(ch == 'b') freq[0]++;
+            if(ch == 'a') freq[1]++;
+            if(ch == 'l') freq[2]++;
+            if(ch == 'o') freq[3]++;
+            if(ch == 'n') freq[4]++;
+        }
+        int ans = INT_MAX;
+        freq[2]/=2; freq[3]/=2;
+        for(int el : freq){
+            if(el < ans) ans = el;
+        }
+        return ans;
     }
 };
