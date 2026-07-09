56e9bd856b5bbcc3344e68a3b1cf6878dfea3873
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-07-09 10:39:56 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp = vector<vector<int>>(101, vector<int>(101, -1));
-    int path(int m, int n){
-        if(m < 0 || n < 0) return 0;
-        if(m == 0 && n == 0) return dp[m][n] = 1;
-        if(dp[m][n] != -1) return dp[m][n];
-        return dp[m][n] = path(m, n-1) + path(m-1, n);
-    }
+    // int path(int m, int n) {
+    //     if (m < 0 || n < 0)
+    //         return 0;
+    //     if (m == 0 && n == 0)
+    //         return dp[m][n] = 1;
+    //     if (dp[m][n] != -1)
+    //         return dp[m][n];
+    //     return dp[m][n] = path(m, n - 1) + path(m - 1, n);
+    // }
     int uniquePaths(int m, int n) {
-        return path(m-1, n-1);
+        vector<vector<int>> dp(m,
+                               vector<int>(n, 1)); // Fill first row/col with 1
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
